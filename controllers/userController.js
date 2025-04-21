182562b55a653edcf89e05ac5a58dc63a2e5b057
Rajeev-patel <patelrajeev10342@gmail.com>
2025-04-21 14:52:08 +0530

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -292,7 +292,7 @@ exports.register = async (req, res) => {
         const { username, email, phoneNumber, password } = req.body;
 
         let user = await User.findOne({ email });
-        if (user) return res.status(400).json({ message: "User already exists" });
+        if (user) return res.status(200).json({ message: "User already exists" });
         
         const hashedPassword = await bcrypt.hash(password, 10);
         // const otp = generateOTP();
