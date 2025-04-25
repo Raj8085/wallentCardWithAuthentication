e591b8b59a9f1a14655bf6976fe58b70bf131cdc
Rajeev-patel <patelrajeev10342@gmail.com>
2025-04-25 15:46:48 +0530

    updated all the things

--- models/User.js
+++ models/User.js
@@ -1,5 +1,41 @@
-// models/User.js
+// // models/User.js
 const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     username: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//     },
+//     email : {
+//       type : String,
+//       required : true,
+//       unique : true
+//     },
+//     phoneNumber:{
+//       type:Number,
+//       required:true,
+//   },
+//   verifyOtp:{
+//       type:Number,
+//   },
+//   sendOtp:{
+//       type : String,
+//   },
+//   mailOtp :{
+//     type : String,
+//   }
+//   },
+//   { timestamps: true }
+// );
+// module.exports = mongoose.model("User", userSchema);
+
+
 
 const userSchema = new mongoose.Schema(
   {
@@ -12,25 +48,26 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    email : {
-      type : String,
-      required : true,
-      unique : true
+    email: {
+      type: String,
+      required: true,
+      unique: true
     },
-    phoneNumber:{
-      type:Number,
-      required:true,
-  },
-  verifyOtp:{
-      type:Number,
-  },
-  sendOtp:{
-      type : String,
-  },
-  mailOtp :{
-    type : String,
-  }
+    phoneNumber: {
+      type: String, // Change to String for consistency
+      required: true,
+    },
+    verifyOtp: {
+      type: String, // Change to String for consistency
+    },
+    sendOtp: {
+      type: String,
+    },
+    mailOtp: {
+      type: String,
+    }
   },
   { timestamps: true }
 );
+
 module.exports = mongoose.model("User", userSchema);
